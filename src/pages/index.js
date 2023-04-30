c3afca090fb1cce02f6e785348c7a649bb3ca73b
Skultty <samuel.skultety@outlook.sk>
2023-04-30 16:46:13 +0200

    fix: import path adjustment

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
-import Navbar from "../components/Navbar";
-import Main from "../components/Main";
-import About from "../components/About";
-import Projects from "../components/Projects";
+import Navbar from "../components/Navbar.jsx";
+import Main from "../components/Main.jsx";
+import About from "../components/About.jsx";
+import Projects from "../components/Projects.jsx";
 
 export default function Home() {
   return (
